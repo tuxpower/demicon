0c71045341ab13060bce1746348be4bb6d0353f6
Jose Gaspar <jad.gaspar@gmail.com>
2022-12-03 16:44:08 +0000

    remove comment

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -7,7 +7,6 @@ s3 = boto3.resource('s3')
 def lambda_handler(event, context):
     
     s3.meta.client.download_file('josegaspar-terraform-state', 'terraform.tfstate', '/tmp/terraform.tfstate')
-    #sp = subprocess.call(['./terraform', 'output', '-state', '/tmp/terraform.tfstate', event['resource']])
     sp = subprocess.Popen(['./terraform', 'output', '-state', '/tmp/terraform.tfstate', event['resource']], stdout=subprocess.PIPE)
     
     return {
